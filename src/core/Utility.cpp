3d6ecdf24a9ba9ae319735ac64629f58c77bfba8
Nevermore1994 <875229565@qq.com>
2020-10-09 21:11:50 +0800

    fix bug

--- src/core/Utility.cpp
+++ src/core/Utility.cpp
@@ -37,7 +37,7 @@ std::string getNowTime()
 {
     tm tm_time;
     char t_time[32];
-    int len  = snprintf(t_time, sizeof (t_time), "%4d%02%02d %02d:%02d:%02d",
+    int len  = snprintf(t_time, sizeof (t_time), "%4d%02d%02d %02d:%02d:%02d",
             tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
             tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
     return std::string(t_time);
