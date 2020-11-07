c15ec7022b5a17ad50853d005851c5cbfdfddc80
Nevermore1994 <875229565@qq.com>
2020-11-07 22:18:43 +0800

    update grammar

---
+++ src/core/Grammar.cpp
@@ -0,0 +1,199 @@
+#include"Grammar.h"
+#include"Symbol.h"
+
+using namespace T; 
+
+Grammar::Grammar(Compiler & compiler)
+	:manager_(compiler)
+{
+}
+
+void Grammar::translationUnit()
+{
+	while(manager_.getLex()->getToken() != TK_EOF)
+	{
+		externalDeclaration(StorageClass::T_GLOBAL);
+	}
+}
+
+void Grammar::externalDeclaration(StorageClass level)
+{
+
+}
+
+SymbolTypePointer Grammar::typeSpecifier()
+{
+	SymbolTypePointer res;
+	TypeCode t = TypeCode::T_UNDEFINED;
+	int token_ = manager_.getLex()->getToken();
+	SymbolPointer ref;
+	switch (token_)
+	{
+		case KW_CHAR:
+			t = TypeCode::T_CHAR;
+			break;
+		case KW_SHORT:
+			t = TypeCode::T_SHORT;
+			break;
+		case KW_VOID:
+			t = TypeCode::T_VOID;
+			break;
+		case KW_INT:
+			t = TypeCode::T_INT;
+			break;
+		case KW_DOUBLE:
+			t = TypeCode::T_DOUBLE;
+			break;
+		case KW_FLOAT:
+			t = TypeCode::T_FLOAT;
+			break;
+		case KW_STRUCT:
+			t = TypeCode::T_STRUCT;
+			ref = structSpecifier();
+			break;
+	}
+	if (t != TypeCode::T_UNDEFINED)
+	{
+		if (ref == nullptr)
+			res = std::make_shared<SymbolType>(static_cast<int>(t));
+		else
+			res = std::make_shared<SymbolType>(static_cast<int>(t), ref);
+		syntaxState_ = SyntaxState::SNTX_SP;
+	}
+	if (token_ != KW_STRUCT)
+	{
+		manager_.getLex()->handleToken();
+	}
+	return res;
+}
+
+SymbolPointer Grammar::structSpecifier()
+{
+	return nullptr;
+}
+
+void Grammar::structDeclarationList(SymbolTypePointer type)
+{
+}
+
+void Grammar::structDeclaration(int * maxalign, int * offset, SymbolPointer & ps)
+{
+}
+
+void Grammar::declarator(SymbolTypePointer type, int * v, int * forceAlign)
+{
+}
+
+void Grammar::functionCallingConvention(int * fc)
+{
+}
+
+void Grammar::structMemberAlignment(int * forceAlign)
+{
+}
+
+void Grammar::directDeclarator(SymbolTypePointer type, int * v, int funcCall)
+{
+}
+
+void Grammar::directDeclaratorPostfix(SymbolTypePointer type, int funcCall)
+{
+}
+
+void Grammar::parameterTypeList(SymbolTypePointer type, int funcCall)
+{
+}
+
+void Grammar::funcbody(SymbolPointer sym)
+{
+}
+
+int Grammar::isTypeSpecifier(int id)
+{
+	return 0;
+}
+
+void Grammar::statement(int * bsym, int * csym)
+{
+}
+
+void Grammar::compoundStatement(int * bsym, int * csym)
+{
+}
+
+void Grammar::ifStatement(int * bsym, int * csym)
+{
+}
+
+void Grammar::forStatement(int * bsym, int * csym)
+{
+}
+
+void Grammar::continueStatement(int * csym)
+{
+}
+
+void Grammar::breakStatement(int * bsym)
+{
+}
+
+void Grammar::returnStatement()
+{
+}
+
+void Grammar::expressionStatement()
+{
+}
+
+void Grammar::expression()
+{
+}
+
+void Grammar::assignmentExpression()
+{
+}
+
+void Grammar::equalityExpression()
+{
+}
+
+void Grammar::relationalExpression()
+{
+}
+
+void Grammar::additiveExpression()
+{
+}
+
+void Grammar::multiplicativeExpression()
+{
+}
+
+void Grammar::unaryExpression()
+{
+}
+
+void Grammar::sizeofExpression()
+{
+}
+
+void Grammar::postfixExpression()
+{
+}
+
+void Grammar::primaryExpression()
+{
+}
+
+void Grammar::argumentExpressionList()
+{
+}
+
+void Grammar::printTab(int num)
+{
+}
+
+void Grammar::syntaxIndent()
+{
+}
+
--- src/core/Grammar.h
+++ src/core/Grammar.h
@@ -2,9 +2,13 @@
 
 #include<string>
 #include"Symbol.h"
+#include"Lex.h"
+#include"Compiler.h"
 
 namespace T
 {
+
+class Compiler;
 
 enum class SyntaxState
 {
@@ -27,7 +31,7 @@ enum class StorageClass
 	T_ANOM = 0x10000000,  //anonymous sign
 	T_STRUCT = 0x20000000, 
 	T_MEMBER = 0x40000000, 
-	T_PARAMS = 0x80000000,  
+	T_PARAMS = 0x60000000,  
 };
 
 enum TypeCode
@@ -35,13 +39,16 @@ enum TypeCode
 	T_INT = 0,
 	T_CHAR = 1,
 	T_SHORT = 2,
-	T_VOID = 3,
-	T_PTR = 4,
-	T_FUNC = 5,
-	T_STRUCT = 6,
+	T_FLOAT = 3,
+	T_DOUBLE = 4,
+	T_VOID = 5,
+	T_PTR = 6,
+	T_FUNC = 7,
+	T_STRUCT = 8,
 
 	T_BTYPE = 0x000f,
 	T_ARRAY = 0x0010,
+	T_UNDEFINED = 0xffff,
 };
 
 constexpr int ALIGNSET = 0x100;
@@ -49,13 +56,14 @@ constexpr int ALIGNSET = 0x100;
 class Grammar
 {
 public:
+	Grammar(Compiler& compiler);
     SyntaxState getSyntaxState() const { return syntaxState_; }
     int getgetSyntaxLevel() const { return syntaxLevel_;}
-    void translationUnit(void);
-    void externalDeclaration(int level);
+    void translationUnit();
+    void externalDeclaration(StorageClass level);
     //void initializer(SymbolType* ptype,  int c, Section* psec); 
-    int typeSpecifier(SymbolTypePointer type);
-    void structSpecifier(SymbolTypePointer type);
+	SymbolTypePointer typeSpecifier();
+	SymbolPointer structSpecifier();
     void structDeclarationList(SymbolTypePointer type);
     void structDeclaration(int* maxalign, int* offset, SymbolPointer& ps);
     void declarator(SymbolTypePointer type, int* v, int* forceAlign);
@@ -90,6 +98,7 @@ public:
 private:
     SyntaxState syntaxState_;
     int syntaxLevel_;
+	Compiler& manager_;
 };
 
 }//end namespace T
